08126d59dcd5b2586680c534e208b9dc37038162
Marcel Eggert <57983054+pommernMeg@users.noreply.github.com>
2025-03-01 15:37:50 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,13 @@
+from utils.summary import generate_summary
+from utils.utilities import utilities_ui
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+# Main Streamlit App
+st.title("SQLite Reading Summary Generator")
+
+# Tabs for navigation
+tabs = st.tabs(["Summary", "Utilities"])
+
+with tabs[0]:
+    st.write("### Summary")
+    generate_summary()
